5aacd575160820518b8b998719b298661f33037c
Alibadaraguindo <153864729+Alibadaraguindo@users.noreply.github.com>
2025-05-22 15:31:28 +0100

    Update Tp21Application.java

--- TP2_1/src/main/java/net/ali/tp2_1/Tp21Application.java
+++ TP2_1/src/main/java/net/ali/tp2_1/Tp21Application.java
@@ -21,14 +21,12 @@ public class Tp21Application {
     @Bean
     CommandLineRunner start(PatientRepository patientRepository) {
         return args -> {
-            // Ajouter quelques patients normaux
             patientRepository.save(builder().Nom("Ali").dateNaissance(new Date()).malade(true).score(100).build());
             patientRepository.save(builder().Nom("Fatima").dateNaissance(new Date()).malade(false).score(120).build());
             patientRepository.save(builder().Nom("Mohamed").dateNaissance(new Date()).malade(true).score(80).build());
             patientRepository.save(builder().Nom("Khadija").dateNaissance(new Date()).malade(false).score(90).build());
             patientRepository.save(builder().Nom("Omar").dateNaissance(new Date()).malade(true).score(110).build());
 
-            // Ajouter quelques patients "Maliens"
             patientRepository.save(builder().Nom("Mamadou (Mali)").dateNaissance(new Date()).malade(false).score(105).build());
             patientRepository.save(builder().Nom("Aminata (Mali)").dateNaissance(new Date()).malade(true).score(95).build());
             patientRepository.save(builder().Nom("Seydou (Mali)").dateNaissance(new Date()).malade(false).score(88).build());
